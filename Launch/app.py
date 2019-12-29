84d189c76b630ac522e74893ca9f1e9032c0ba40
TL <tom.kennedy.lee@gmail.com>
2019-12-28 20:14:07 -0500

    testing new features

--- Launch/app.py
+++ Launch/app.py
@@ -15,11 +15,6 @@ import tables
 
 app = Flask(__name__)
 
-# DATABASE_URL will contain the database connection string:
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
-# Connects to the database using the app config
-db = SQLAlchemy(app)
-
 
 @app.route('/')
 def index():
